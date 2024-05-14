51854a84d3818eddc4b1204ab2fc1464975249c6
Sudeep Takkar <148792635+st-tsl@users.noreply.github.com>
2024-05-14 19:15:42 -0400

    Week 2 work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
-import {Component, useEffect, useState } from 'react';
+// import {Component, useEffect, useState } from 'react';
+import Counter from './components/Counter'
+import UseEffectExample1 from './components/UseEffectExample1'
+import UseEffectExample2 from './components/UseEffectExample2'
+import UseEffectExample3 from './components/UseEffectExample3';
 function App() {
-  
-  const [todos, setTodos] = useState([])
-
-  useEffect(() => {
-    let newTodos = [
-      {task: 'Buy an iphone', isCompleted: true},
-      {task: 'Buy groceries', isCompleted: false},
-    ]
-    setTodos(newTodos)
-  }, [])
   return (
     <div className="App">
-      <ul style={{listStyleType: 'none'}}>
-        {todos.map(todo => {
-          return <li>{todo.task}</li>
-        })}
-      </ul>
+      {/* <Counter />
+      <UseEffectExample1 />
+      <UseEffectExample2 /> */}
+      <UseEffectExample3 />
     </div>
   );
 }
