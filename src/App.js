664a00aa5b49d6c01162e01667b85357d96c277e
st-tsl <148792635+st-tsl@users.noreply.github.com>
2024-05-14 18:47:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Component, useEffect, useState } from 'react';
 function App() {
+  
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    let newTodos = [
+      {task: 'Buy an iphone', isCompleted: true},
+      {task: 'Buy groceries', isCompleted: false},
+    ]
+    setTodos(newTodos)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul style={{listStyleType: 'none'}}>
+        {todos.map(todo => {
+          return <li>{todo.task}</li>
+        })}
+      </ul>
     </div>
   );
 }
