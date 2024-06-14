176e81537c30ed1812284261cdd4064424a5a82f
Sudeep Takkar <148792635+st-tsl@users.noreply.github.com>
2024-06-13 21:05:26 -0400

    week 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import "./App.css";
 import { useState } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+    query ExampleQuery {
+      getAllTodos {
+        id
+        task
+        isCompleted
+      }
+    }
+    `,
+  })
+  .then((result) => console.log(`result from graphql - ${JSON.stringify(result)}` ));
 
 function App() {
   const todosList = [
@@ -15,41 +35,43 @@ function App() {
 
   function deleteTodo(id) {
     // IMPLEMENT DELETE TODO
-    const updatedTodoList = todos.filter(todo => {
-      return todo.id !== id
-    })
-    setTodos(updatedTodoList)
+    const updatedTodoList = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(updatedTodoList);
   }
 
   function addTodo(newTodo) {
     const updatedTodoList = [...todos, newTodo];
-    setTodos(updatedTodoList)
-    debugger
+    setTodos(updatedTodoList);
+    debugger;
     // IMPLEMENT ADD TODOS
   }
 
   function editTodo(id, updatedTodo) {
     // IMPLEMENT EDIT TODO
-    const updatedTodoList = todos.map(todo => {
-      return todo.id === id ? updatedTodo : todo
+    const updatedTodoList = todos.map((todo) => {
+      return todo.id === id ? updatedTodo : todo;
     });
-    debugger
-    setTodos(updatedTodoList)
+    debugger;
+    setTodos(updatedTodoList);
   }
 
   return (
-    <VStack p={5}>
-      <Text
-        bgGradient="red"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-      >
-        Todo App
-      </Text>
-      <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
-      <AddTodo addTodo={addTodo} />
-    </VStack>
+    <ApolloProvider client={client}>
+      <VStack p={5}>
+        <Text
+          bgGradient="red"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+        >
+          Todo App
+        </Text>
+        <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+        <AddTodo addTodo={addTodo} />
+      </VStack>
+    </ApolloProvider>
   );
 }
 
